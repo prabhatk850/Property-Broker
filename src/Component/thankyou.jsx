2e16b73bf90172dffeebcd8d8406424a85f04aab
Prabhat <prabhatk850@gmail.com>
2024-01-16 16:26:46 +0530

    Add styled component and redirect on Thankyou page

--- src/Component/thankyou.jsx
+++ src/Component/thankyou.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+`;
 
 function Thankyou() {
-  return (
-    <div>Thankyou</div>
-  )
+    setTimeout(() => {window.location.href = "/"}, 3000); 
+  
+    return (
+        <Wrapper>
+            <img  src="./Thankyou.png" alt="Thank you" />
+        </Wrapper>
+    )
 }
 
 export default Thankyou
